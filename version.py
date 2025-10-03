7a85b62bd1e356e69d2101b32e41586e1508236f
arshad abbas <arshadblouch81@gmail.com>
2025-10-03 15:46:42 +0500

    new changes to fix issue

---
+++ version.py
@@ -0,0 +1,19 @@
+import importlib.metadata
+packages = [
+   
+    "python-dotenv",
+    "fastmcp",
+    "aiosqlite",
+    "fastapi",
+    
+]
+for pkg in packages:
+    try:
+        version = importlib.metadata.version(pkg)
+        print(f"{pkg}=={version}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{pkg} (not installed)")
+
+# # serve static & templates
+# app.mount("/static", StaticFiles(directory="../static"), name="static")
+# templates = Jinja2Templates(directory="../templates")
